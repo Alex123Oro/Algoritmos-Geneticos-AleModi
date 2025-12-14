e54bd3ca8624402334a50e3c82390b72100a91a3
VictoriaGrageda <victoriagragedavallejos@gmail.com>
2025-12-13 23:02:27 -0400

    update changes client and admin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,17 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   const PORT = process.env.PORT ?? 3001;
 
   await app.listen(PORT);
-  console.log(`🚀 Backend corriendo en http://localhost:${PORT}`);
+  console.log(`Backend corriendo en http://localhost:${PORT}`);
 }
 
 bootstrap();
